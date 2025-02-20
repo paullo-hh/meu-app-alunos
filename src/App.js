87cfe2fefb06baedbd22c73e35bb888bfe4cdad8
Paulo H. Carvalho <contacthenry96@gmail.com>
2025-02-19 22:40:36 -0300

    Iniciando minha aprendizagem de React

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Estude React
         </a>
       </header>
     </div>
